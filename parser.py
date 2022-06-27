948a9d5c506576454be23b727d2d0c5471fd12a1
OmarAbushaban <omarghalib@gmail.com>
2022-06-28 02:45:46 +0300

    improved json output

--- parser.py
+++ parser.py
@@ -4,8 +4,8 @@ import re
 
 def read_all_files_and_parse_to_dict(folderPath, fileName):
   subfolders = [ f.path for f in os.scandir(folderPath) if f.is_dir() ]
-  # a Python object (dict):
   dict_object = {}
+  songs_dict = {}
   list_of_chord_sets = []
   node_id = 0
   for subfolder in subfolders:
@@ -22,8 +22,9 @@ def read_all_files_and_parse_to_dict(folderPath, fileName):
     sub_dict["title"] = get_title(salami_chords)
     sub_dict["artist"] = get_artist(salami_chords)
     sub_dict["chords"] = chords_dict
-    dict_object[node_id] = sub_dict
+    songs_dict[node_id] = sub_dict
     node_id = node_id + 1
+  dict_object["songs"] = songs_dict
   return [dict_object, list_of_chord_sets]
 
 def get_chord_set(text):
@@ -44,20 +45,22 @@ def save_json_to_file(filePath, jsonString):
   with open(filePath, 'w') as outfile:
     outfile.write(jsonString)
 
-def update_dict_with_similarity_scores(songs_dict, list_of_chord_sets):
+def update_dict_with_similarity_scores(output_dict, list_of_chord_sets):
   song_id = 0
+  scores_dict = {}
   for chord_set in list_of_chord_sets:
-    songs_dict[song_id]["similarityScores"] = {}
+    scores_dict[song_id] = {"similarityScores": {}}
     other_song_id = song_id+1
     for other_chord_set in list_of_chord_sets[other_song_id:]:
       similarity_score = calculate_similarity_score(chord_set, other_chord_set)
-      if "similarityScores" not in songs_dict[other_song_id]:
-        songs_dict[other_song_id]["similarityScores"] = {}
-      songs_dict[song_id]["similarityScores"][other_song_id] = {"score": similarity_score, "songId": other_song_id}
-      songs_dict[other_song_id]["similarityScores"][song_id] = {"score": similarity_score, "songId": song_id}
+      if other_song_id not in scores_dict:
+        scores_dict[other_song_id] = {"similarityScores": {}}
+      scores_dict[song_id]["similarityScores"][other_song_id] = {"score": similarity_score, "songId": other_song_id}
+      scores_dict[other_song_id]["similarityScores"][song_id] = {"score": similarity_score, "songId": song_id}
       other_song_id = other_song_id + 1
     song_id = song_id + 1
-  return songs_dict
+  output_dict["scores"] = scores_dict
+  return output_dict
     
 def calculate_similarity_score(chord_set, other_chord_set):
   difference = chord_set.difference(other_chord_set)
@@ -69,9 +72,9 @@ def convert_dict_to_json(dict_object):
   return json_object
 
 def main():
-    [songs_dict, list_of_chord_sets] = read_all_files_and_parse_to_dict("/content/drive/MyDrive/Colab Notebooks/McGill-Billboard", "salami_chords.txt")
-    songs_dict = update_dict_with_similarity_scores(songs_dict, list_of_chord_sets)
-    json_tree = convert_dict_to_json(songs_dict)
+    [output_dict, list_of_chord_sets] = read_all_files_and_parse_to_dict("/content/drive/MyDrive/Colab Notebooks/McGill-Billboard", "salami_chords.txt")
+    output_dict = update_dict_with_similarity_scores(output_dict, list_of_chord_sets)
+    json_tree = convert_dict_to_json(output_dict)
     save_json_to_file("/content/drive/MyDrive/McGill_Billboard.json",json_tree)
 
 if __name__ == "__main__":
